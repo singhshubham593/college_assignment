a6e76c3a8f7d5f12e3ccad7fb658d28cfff24e04
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-12 20:41:57 +0530

    c2

---
+++ C2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	float b;
+	char c;
+	
+	printf("Enter an integer \n");
+	scanf("%d",&a);
+	printf("Enter a real no \n ");
+	scanf("%f",&b);
+	printf("Enter a charecter \n ");
+	scanf("%c",&c);
+	
+	printf("integer is : %d/n",a);
+	printf("real no is is : %f/n",b);
+	printf("charecter is : %c/n",c);
+	
+	return 0;
+}
